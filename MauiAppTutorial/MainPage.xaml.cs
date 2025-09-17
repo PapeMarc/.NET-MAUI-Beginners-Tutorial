24d79f524dd614ab90676fe0471b1b40ecdea206
Marc Pape <marc.pape-info@web.de>
2025-09-17 21:11:10 +0200

    Added ViewModel (MVVM) and Data Binding like shown in the ".NET MAUI for Beginners" Tutorial on Youtube.

--- MauiAppTutorial/MainPage.xaml.cs
+++ MauiAppTutorial/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiAppTutorial
+﻿using MauiAppTutorial.ViewModel;
+
+namespace MauiAppTutorial
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        public MainPage(MainViewModel vm)
         {
             InitializeComponent();
+            BindingContext = vm;
         }
     }
 }
